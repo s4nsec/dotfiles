85dd015479fc912f76c762695170bc7c33dbacd5
vjabrayilov <vjabrayilov@cs.columbia.edu>
2026-07-16 17:25:47 +0400

    chores(nvim): Add snacks keybindings

--- nvim/.config/nvim/lua/vjabrayilov/init.lua
+++ nvim/.config/nvim/lua/vjabrayilov/init.lua
@@ -38,7 +38,6 @@ autocmd('LspAttach', {
     group = vjabrayilov,
     callback = function(e)
         local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -54,4 +53,3 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
