46d86affe177572889a182a1cdf31174cfab2f6b
Sanan Hasanov <sanan.hasanov@ucf.edu>
2026-07-28 20:34:56 +0400

    minor nits

--- nvim/.config/nvim/lua/vjabrayilov/lazy/leap.lua
+++ nvim/.config/nvim/lua/vjabrayilov/lazy/leap.lua
@@ -1,7 +1,8 @@
 return {
     "ggandor/leap.nvim",
     config = function()
-        require("leap").create_default_mappings()
+        vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+        vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+        vim.keymap.set({ "n", "x", "o" }, "<C-s>", "<Plug>(leap-backward)")
     end,
-    vim.keymap.set({'n', 'x', 'o'}, '<C-s>', '<Plug>(leap-backward)')
 }
